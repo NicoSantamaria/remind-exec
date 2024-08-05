15d84397c48268f31a7d43454b448250b5f7bd3f
NicoSantamaria <nicoasantamaria@gmail.com>
2024-08-05 01:56:15 -0300

    passing props to List component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import List from './sections/List';
 const TEST_USER_ID: number = 1;
 
 const App: React.FC = () => {
-	const [lists, setLists] = useState<ListItems | null>(null);
+	const [lists, setLists] = useState<ListItems[] | null>(null);
+	const [userId, setuserId] = useState<number>(TEST_USER_ID);
 	const [loading, setLoading] = useState<boolean>(true);
 
 	useEffect(() => {
@@ -21,12 +22,22 @@ const App: React.FC = () => {
 			setLoading(false);
 		}
 		getLists();
-	})
+	}, [loading]);
 
-	return (
-		<main className="flex justify-between h-full w-full">
+	return ( loading 
+		? <p>Loading...</p>
+		: <main className="flex justify-between h-full w-full">
+
 			<section className="w-full border border-black rounded m-5">
-				<List user_id={TEST_USER_ID} />
+				{lists?.map((item) => (
+					<div>
+						<h1>{item.list_name}</h1>
+						<List 
+							user_id={userId} 
+							list_data={item.list_data}
+						/>
+					</div>
+				))}
 			</section>
 
 			<section className="w-full border border-black rounded m-5">
@@ -36,6 +47,7 @@ const App: React.FC = () => {
 			<section className="w-full border border-black rounded m-5">
 				Calendar
 			</section>
+			
 		</main>
 	);
 }
