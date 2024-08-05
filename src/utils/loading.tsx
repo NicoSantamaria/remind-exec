f72db17c5ff9ecddd3abc82ecde572fd86ccfaa8
NicoSantamaria <nicoasantamaria@gmail.com>
2024-08-05 02:10:42 -0300

    skeleton for loading design

---
+++ src/utils/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loading: React.FC = () => {
+    // I can put a beautiful loading design here when 
+    // I get around to styling 
+    return (
+        <p>
+            Loading...
+        </p>
+    )
+}
+
+export default Loading;
