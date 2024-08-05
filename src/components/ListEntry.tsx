3c20ba3384bd9fb87eb1b6334eff021bab457e0d
NicoSantamaria <nicoasantamaria@gmail.com>
2024-08-05 08:27:32 -0300

    ListEntry scaffolding

--- src/components/ListEntry.tsx
+++ src/components/ListEntry.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Task } from '../utils/types';
+import Loading from '../utils/Loading';
 
 type ListEntryProps = {
 	user_id: number,
@@ -8,11 +9,20 @@ type ListEntryProps = {
 }
 
 const ListEntry: React.FC<ListEntryProps> = ({ user_id, parent_id, first_layer }) => {
+	const [tasks, setTasks] = useState<Task[] | null>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		// fetch tasks
+		setLoading(false);
+	}, [loading]);
+
 	// Call to grab next layer from user_id and parent_id
 	// Parent_id will refer to list table if first_layer == true, else
 	// refers to tasks table
-	return (
-		<div>
+	return (loading
+		? <Loading />
+		: <div>
 			<p>
 				{user_id}
 			</p>
