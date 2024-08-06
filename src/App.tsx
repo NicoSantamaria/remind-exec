b1413a8033d0a4777bd04dbc97a61831c721c4fa
NicoSantamaria <nicoasantamaria@gmail.com>
2024-08-06 15:38:29 -0700

    whitespace

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
 		getLists();
 	}, [userId]);
 
-	return ( loading 
+	return (loading 
 		? <Loading />
 		: <main className="flex justify-between h-full w-full">
 			<section className="w-full border border-black rounded m-5">
